8c4d060a4645e17280b4aa2cf5c7ac2afc71a670
MStanchev <MStanchev@MStanchev-NB.luxoft.com>
2018-11-13 18:12:41 +0200

    Implemented website grabber and generation of channel links list.

--- Player/main.py
+++ Player/main.py
@@ -4,6 +4,15 @@ Created on 9 Nov 2018
 @author: MStanchev
 '''
 
+from Player import SiteGrabber
+
+
 if __name__ == '__main__':
+
     print "Main Program"
-    pass
+    
+    SG = SiteGrabber.Grabber()
+    
+    SG.get_channel_list()
+    
+    
